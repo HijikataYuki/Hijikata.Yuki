9b3b05e9f252740522e72b2fd2caa91e032f4095
hijikatayuki0129 <64465030+hijikatayuki0129@users.noreply.github.com>
2020-09-14 19:09:23 +0900

    Add files via upload

--- LessonController27.java
+++ LessonController27.java
@@ -14,73 +14,82 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 演習27：バブルソート
  * @author hijikata.yuki
- *
  */
 @SpringBootApplication
 @RestController
 public class LessonController27 {
 
-	Random		rnd = new Random();		// 乱数オブジェクトの生成（種の生成）
+	Random rnd = new Random();// 乱数オブジェクトの生成（種の生成）
+	/**
+	 * バブルソートをする
+	 * @return
+	 */
 	@RequestMapping("/hijikata/ex027")
-	public String main(){
+	public String doBubbleSort(){
 
 		StringBuilder out = new StringBuilder();
-
-		int array[]= new int[10];
-		int a = 100;             // 乱数生成用
-		int	b;						// 乱数値
-		int box = 0;                         // 仮格納用
-
+		//配列 ＝ array
+		int randomArray[] = new int[10];  //乱数を格納するための配列(10個分)
+		final int randomMax = 100;  //乱数の最大値(0-99) 定数
 
 		out.append("↓乱数生成と格納");
 		out.append("<br/>");
 
 
-		//乱数を配列に格納
-		for (int i=0;  i<array.length; i++) {
-			b = subMethod(a);               // 0～99までの乱数を発生
-			out.append(b + "　　　" );
-			array[i]=b;
-		}
+		//乱数を配列に格納する
+		for (int i = 0;  i < randomArray.length; i ++ ) {
+			int randomNumber;  //乱数値
+			randomNumber = getRandom(randomMax);               //0～99までの乱数を発生
+			out.append(randomNumber).append("　　　");  //乱数の区切りのために空白を挿入する
+			randomArray[i] = randomNumber;
+		}//乱数を配列に格納済み
 
 		out.append("<br/>");
 		out.append("<br/>");
 
 
-		//ソートが完了するまでのループ
+		//ソートが完了するまでのループ(大ループ)
 
-		//2以上の間は実行する、1以下で終了
-		for (int fin=array.length; fin>=2; fin--) {
+		//配列最大値-1回繰り返す(一回につき一つの数字の位置が確定する)
+		for (int j = 0; j < randomArray.length - 1; j ++ ) {
 
-			//すべての数字を回るループ
-			for (int i=1; i<array.length; i++) {
+			//前後の数字を比較するループを行う(小ループ)
+			//(配列最大値-1-大ループの回数)回繰り返す(大ループ一回につき数字の配置が一つ確定しているから)
+			for (int i = 0; i < randomArray.length - 1 - j; i ++ ) {
 
-				//前の数字と比較して並び替える判定
-				if (array[i-1] > array[i]) {
-					box = array[i]; //手前の数字を箱へ格納
-					array[i] = array[i-1];  //奥の数字を手前へ
-					array[i-1] = box;  //箱から奥の数字へ元・手前の数字を格納(入れ替え完了)
+				//配列の前後の要素と比較して前が大きい場合は入れ替える
+				if ( randomArray[i] > randomArray[i + 1] ) {
+					int temp;  //仮格納用の変数を宣言することで入れ替える際に使用する
+					temp = randomArray[i] ;  //宣言した仮格納用の変数に一時的に前の数字を格納する
+					randomArray[i] = randomArray[i + 1];  //前の数字が格納されている箇所に後の数字を格納(前方へ後の数字を移動させたことになる)
+					randomArray[i + 1] = temp;  //後の数字が格納されている箇所に、仮格納用の変数に一時的に格納されている前の数字を格納する(後方へ前の数字を移動させたことになる)
 				}
 			}
 
 		}
 
-		//バブルソート完了
+		//バブルソート完了後の配列の中身を表示
 		out.append("<br/>");
 		out.append("↓バブルソート実行後");
 		out.append("<br/>");
-		for (int i=0; i<array.length; i++) {
-			out.append(array[i] + "　　　" );
+		for (int i = 0; i < randomArray.length; i ++ ) {
+			out.append(randomArray[i]).append("　　　");//乱数の区切りのために空白を挿入する
 		}
 		return out.toString();
 	}
 
 	//乱数生成用メソッド
-	public int subMethod(int val) {
-		int c=val;
-		c = rnd.nextInt(val);
-		return c;
+	/**
+	 * 乱数生成用メソッド
+	 * @param val 乱数の最大値
+	 * @return 乱数
+	 */
+	public int getRandom(int val) {
+		int rundomNumber;
+		rundomNumber = rnd.nextInt(val);
+		return rundomNumber;
 	}
 
 }
